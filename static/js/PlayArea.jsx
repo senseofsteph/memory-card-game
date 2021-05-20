c0692acd174ef2135e4abcba8c0a2ad77d28992b
Stephanie <stephogam@gmail.com>
2021-05-20 20:04:40 +0000

    Fix react fragment to return card properly instead of hardcode only

--- static/js/PlayArea.jsx
+++ static/js/PlayArea.jsx
@@ -7,14 +7,16 @@ function PlayArea() {
   .then((data) => {
     console.log(data);
     tempData = data[0];
-    console.log(tempData);
+    console.log(tempData.word);
     // cards.push(<Card color=data.color word=data.word />)
     
   })
 
   return(
     <React.Fragment>
-      <Card color={tempData.color} word={tempData.word} />
+      {/* <Card id={tempData.id} color={tempData.color} word={tempData.word} /> */}
+      <Card id='0a' color='blue' word= 'hackbright' />
+      <Card id='0b' color='blue' word='student' />
     </React.Fragment>
   );
 }
