09240c5fbe04739bfa878388864e36c77c47967a
Stephanie <stephogam@gmail.com>
2021-05-22 00:20:59 +0000

    Playing cards populate properly

--- static/js/Main.jsx
+++ static/js/Main.jsx
@@ -1,14 +1,29 @@
 function Main() {
   const [playing, updatePlaying] = React.useState(false);
+  const [deck, updateDeck] = React.useState([]);
+  const [cardsInPlay, updateCardsInPlay] = React.useState([]);
+
+  React.useEffect( () => {
+    fetch("/cards")
+    .then(res => res.json())
+    .then(data => updateDeck(data))
+  }, [])
 
   function initialDeal(){
     updatePlaying(true);
+    updateCardsInPlay(deck.slice(0,16));
+    updateDeck(deck.slice(16, deck.length));
   }
 
   if (playing) {
     return(
     <React.Fragment>
-      <PlayArea />
+      <PlayArea 
+        deck={deck} 
+        updateDeck={updateDeck} 
+        cardsInPlay={cardsInPlay} 
+        updateCardsInPlay={updateCardsInPlay} 
+      />
     </React.Fragment>
     )
   } else {
--- static/js/PlayArea.jsx
+++ static/js/PlayArea.jsx
@@ -1,28 +1,15 @@
-function PlayArea() {
-  const [cardData, updateCardData] = React.useState([]);
-  const [cardsInPlay, updateCardsInPlay] = React.useState([]);
+function PlayArea({deck, updateDeck, cardsInPlay, updateCardsInPlay}) {
   
-
-  React.useEffect( () => {
-    fetch("/cards")
-    .then(res => res.json())
-    .then(data => updateCardData(data))
-  }, [])
-
-
-  // updateCardsInPlay(cardData.slice(0,16));
- 
   return(
     <div id='play-area'>
-      {cardData.map(card => {
+      {cardsInPlay.map(card => {
+        return(
         <Card 
           key={card.id}
           color={card.color}
           word={card.word}
-        />
-      })}
-      <Card id='0a' color='green' word= 'hackbright' />
-      <Card id='0b' color='blue' word='student' />
+        />)
+    })}
     </div>
   );
 }
