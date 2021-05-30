d6823b4e3ef2902306313a0ed96aec9a1f4e8667
Stephanie <stephogam@gmail.com>
2021-05-30 21:30:29 +0000

    Update and style footer

--- static/js/footer.jsx
+++ static/js/footer.jsx
@@ -1,8 +1,9 @@
 function Footer() {
   return(
-    <p>
-      2021 Hackbright Academy | Stephanie Ogamba | Created with extra love and care
-    </p> 
-  )
+    <div>
+      <span class='info'>2021 Hackbright Academy Code Along</span>
+      <span class='info'>Created by Stephanie Ogamba</span>
+    </div>
+  );
 }
 
